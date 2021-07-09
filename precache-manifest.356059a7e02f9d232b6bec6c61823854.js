4a974c49963f1c47d6c4e8d3eddca3d960f2f6ff
umar4gitlab <umar@dts-solution.com>
2021-07-10 00:36:55 +0400

    Updates

---
+++ precache-manifest.356059a7e02f9d232b6bec6c61823854.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35a416df2ee0ba5ae22926acaa2b3c79",
+    "url": "./index.html"
+  },
+  {
+    "revision": "53e96f5087b941cc77a1",
+    "url": "./static/css/2.857c08d5.chunk.css"
+  },
+  {
+    "revision": "cb86812e32e0dcb2612a",
+    "url": "./static/css/main.760ced5a.chunk.css"
+  },
+  {
+    "revision": "53e96f5087b941cc77a1",
+    "url": "./static/js/2.a3c40938.chunk.js"
+  },
+  {
+    "revision": "d9fdef230a7dd7148cf528f66d50a43c",
+    "url": "./static/js/2.a3c40938.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0cafc6bb7ad0cbd75e4a",
+    "url": "./static/js/3.37445014.chunk.js"
+  },
+  {
+    "revision": "cb86812e32e0dcb2612a",
+    "url": "./static/js/main.feaa8b35.chunk.js"
+  },
+  {
+    "revision": "423855bf270c6899d5b4",
+    "url": "./static/js/runtime-main.fb66f282.js"
+  },
+  {
+    "revision": "e3413319ca7dec37f7a19187a0c35678",
+    "url": "./static/media/avatar.e3413319.png"
+  },
+  {
+    "revision": "bdb1afe6465bc1fc620e91db28bccfd2",
+    "url": "./static/media/logo.bdb1afe6.png"
+  },
+  {
+    "revision": "7033a675153a034e0345092f671eb678",
+    "url": "./static/media/pdf.7033a675.png"
+  }
+]);
